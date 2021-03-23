1c616ec8bb228533e2978dfa60699f5420ca2197
Charlie Bertrand <charlie.bertrand@live.com>
2021-03-23 17:45:08 +0100

    Scope Role for Actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,8 @@
 class Actor < ApplicationRecord
+  CATEGORY = ["Président", "Acteur", "Trésorier"]
   validates :first_name, presence: :true
   validates :last_name, presence: :true
-  validates :role, collection:
+  validates :role, inclusion: { in: CATEGORY }
   validates :email, uniqueness: :true
   has_one_attached :photo
 end
