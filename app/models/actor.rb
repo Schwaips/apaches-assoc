6f3aafaf838a7c7cae3b695f1624d6f297424f8d
Charlie Bertrand <charlie.bertrand@live.com>
2021-03-23 17:39:41 +0100

    validation on actor model done. And seed for actors done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,7 @@
 class Actor < ApplicationRecord
+  validates :first_name, presence: :true
+  validates :last_name, presence: :true
+  validates :role, collection:
+  validates :email, uniqueness: :true
+  has_one_attached :photo
 end
