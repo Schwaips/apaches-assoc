84dce2683cece14da5837ffcfe39ceae7786a93f
Charlie Bertrand <charlie.bertrand@live.com>
2021-03-24 18:14:04 +0100

    migrate for active storage

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,8 +1,9 @@
 class Actor < ApplicationRecord
-  CATEGORY = ["Président", "Acteur", "Trésorier"]
+  CATEGORY = ["Président", "Acteur", "Trésorier", "Créateur lumière"]
   validates :first_name, presence: :true
   validates :last_name, presence: :true
   validates :role, inclusion: { in: CATEGORY }
   validates :email, uniqueness: :true
   has_one_attached :photo
+  has_many :events
 end
