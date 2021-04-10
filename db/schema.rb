db9fbbc3cc72923a8ad167ca4188719daafc0a95
Charlie Bertrand <charlie.bertrand @live.com>
2021-04-10 16:29:40 +0200

    adding start_time end_time to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_184904) do
+ActiveRecord::Schema.define(version: 2021_04_10_142855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2021_03_27_184904) do
     t.text "description"
     t.text "summary"
     t.text "intention_note"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
