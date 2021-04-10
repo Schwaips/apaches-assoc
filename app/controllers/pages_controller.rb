7d78979414511fe421374f5b7989efc5c64ef2f1
Charlie Bertrand <charlie.bertrand@live.com>
2021-04-10 18:52:33 +0200

    calendar date css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,5 @@ class PagesController < ApplicationController
     @meetings = Event.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
     # Or, for a weekly view:
     @meetings = Event.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
-    
   end
 end
